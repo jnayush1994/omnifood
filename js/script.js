9ff23b8c190126f830ae8650f8d7beecc38e3e93
Ayush Jain <jnayush15101994@gmail.com>
2026-03-09 08:58:09 +0530

    complete mobile navigation and scrolling

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+// Handling mobile navigation
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
